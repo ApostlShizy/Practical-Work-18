70a930aed1291f4451b65db9bc641a9054ec5727
maksim <WalkLU@yandex.ru>
2024-09-23 12:31:45 +0300

    v2

--- Task1.cpp
+++ Task1.cpp
@@ -1,31 +1,29 @@
 #include<iostream>
 #include<vector>
 
-void swapVec(std::vector<int>& vec_0, std::vector<int>& vec_1)
+void swapVec(int array [], std::vector<int>& vec)
 {
-    int count = vec_0.size() > vec_1.size() ? count = vec_1.size() : count = vec_0.size();
-    /*Нахожу меньший вектор на случай если их размер будет отличаться и мы не вышли за предел вектора*/
-    for(int i = 0; i < count;++i)
+    for(int i = 0; i < vec.size();++i)
     {
-        int temp = vec_0[i];
-        vec_0[i] = vec_1[i];
-        vec_1[i] = temp;
+        int temp = array[i];
+        array[i] = vec[i];
+        vec[i] = temp;
     }
 }
 
 int main ()
 {
-    std::vector<int> vec_0 = {1,2,3,4};
-    std::vector<int> vec_1 = {2,4,6,8};
-    swapVec(vec_0,vec_1);
-    std::cout<<"vec_0: ";
-    for(int i = 0; i < vec_0.size();++i)
+    int array [] = {1,2,3,4};    
+    std::vector<int> vec = {2,4,6,8};
+    swapVec(array,vec);  
+    std::cout<<"array: ";
+    for(int i = 0; i < vec.size();++i)
     {
-        std::cout<<vec_0[i]<<" ";
+        std::cout<<array[i]<<" ";
     }    
-    std::cout<<"\nvec_1: ";
-    for(int i = 0;i < vec_1.size();++i)
+    std::cout<<"\nvec: ";
+    for(int i = 0;i < vec.size();++i)
     {
-        std::cout<<vec_1[i]<<" ";
+        std::cout<<vec[i]<<" ";
     }
 }
